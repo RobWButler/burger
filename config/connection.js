e7f0db7f6f687ad995ccf2539b008307f2492994
Robert Butler <robwbutler@gmail.com>
2019-05-27 22:29:16 -0400

    attempt to fix connection error

--- config/connection.js
+++ config/connection.js
@@ -6,13 +6,10 @@
 var mysql = require("mysql");
 
 // Set up our connection information
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} ;
 
 // Connect to the database
 connection.connect(function(err) {
